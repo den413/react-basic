d7476342c272395a43c5a342fa40bb7681831819
den413 <92449901+den413@users.noreply.github.com>
2022-03-08 00:20:37 +0900

    イベント、スタイル

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  const contentstyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>こんばんは</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentstyle}>こんばんは</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
 
 export default App;
-x;
